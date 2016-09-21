496f5f6041fd3b9569bde6035540ea356d59be0e
王立涛 <litao.wang@shouqu.me>
2016-09-21 15:56:28 +0800

    Initial Commit

---
+++ CeckColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CeckColor
+//
+//  Created by 美迅 on 16/9/21.
+//  Copyright © 2016年 ShouQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
